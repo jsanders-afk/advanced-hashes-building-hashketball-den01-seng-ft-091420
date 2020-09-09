bf1bae1de848d40c0e33e8159e56f9a810ef5dfb
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-09 01:52:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,112 @@
-# Write your code here!
+require 'pry'
+
+def game_hash 
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [ {
+                    :player_name =>"Alan Anderson",
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 22,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks => 1
+      },
+                    {
+                    :player_name =>"Reggie Evans",
+                    :number => 30,
+                    :shoe =>  14,
+                    :points => 12,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 12,
+                    :blocks => 12,
+                    :slam_dunks => 7
+      },	
+                    {
+                    :player_name =>"Brook Lopez",
+                    :number => 11,
+                    :shoe => 17,
+                    :points => 17,
+                    :rebounds => 19,
+                    :assists => 10,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks =>15
+      },	
+                    {
+                    :player_name =>"Mason Plumlee",
+                    :number => 1,
+                    :shoe => 19,
+                    :points => 26,
+                    :rebounds => 11,
+                    :assists => 6,
+                    :steals => 3,
+                    :blocks => 8,
+                    :slam_dunks => 5
+      },	
+                    {
+                    :player_name =>"Jason Terry",
+                    :number => 31,
+                    :shoe => 15,
+                    :points => 19,
+                    :rebounds => 2, 
+                    :assists => 2,
+                    :steals => 4,
+                    :blocks => 11,
+                    :slam_dunks => 1
+      }
+                  ]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => []
+    }
+  }
+end
+
+{
+:player_name =>"",
+:number =>"",
+:shoe =>"",
+:points =>"",
+:rebounds =>"",
+:assists =>"",
+:steals => "",
+:blocks =>"",
+:slam_dunks =>""
+}
+
+
+
+
+
+
+
+
+
+
+
+Stat	Info	Info	Info	Info	Info
+Player Name	Alan Anderson	Reggie Evans	Brook Lopez	Mason Plumlee	Jason Terry
+Number	0	30	11	1	31
+Shoe	16	14	17	19	15
+Points	22	12	17	26	19
+Rebounds	12	12	19	11	2
+Assists	12	12	10	6	2
+Steals	3	12	3	3	4
+Blocks	1	12	1	8	11
+Slam Dunks	1	7	15	5	1
+
+
+
+
+
+
+
+
