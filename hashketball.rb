a156c0b5c673db914497fd2999eb99e5f767c8b1
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-09 02:00:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -85,7 +85,7 @@ def game_hash
                     :assists => 7,
                     :steals => 22,
                     :blocks => 15,
-                    :slam_dunks => 0
+                    :slam_dunks => 10
       },	
                     {
                     :player_name => "DeSagna Diop",
